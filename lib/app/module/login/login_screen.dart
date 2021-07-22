0d828acc220d54f2a71901459246190d36d9dc0d
AdemDev <33802588+ademgunay@users.noreply.github.com>
2021-07-22 17:18:53 +0700

    [WIP] Start implementingh login screen

--- lib/app/module/login/login_screen.dart
+++ lib/app/module/login/login_screen.dart
@@ -7,9 +7,66 @@ class LoginScreen extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        body: Center(
-          child: Container(
-            child: Text("Login screen"),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login to Thinky",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              SizedBox(height: 32),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8),
+                child: TextFormField(
+                  autocorrect: false,
+                  onChanged: (newText) {
+                    //TODO
+                  },
+                  //TODO
+                  // validator: (_) => _registerController.emailValidator(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your email',
+                  ),
+                  cursorColor: Colors.purple,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8),
+                child: TextFormField(
+                  obscureText: true,
+                  autocorrect: false,
+                  onChanged: (password) {
+                    //TODO
+                    // _registerController.password.value = password;
+                  },
+                  //TODO
+                  // validator: (_) => _registerController.passwordValidator(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your password',
+                  ),
+                  cursorColor: Colors.purple,
+                ),
+              ),
+              SizedBox(height: 32),
+              ElevatedButton(
+                child: Text("Login"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  textStyle: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  //TODO
+                },
+              ),
+            ],
           ),
         ),
       );
