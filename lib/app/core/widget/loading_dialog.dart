2e63f19b087d3ef04edc681937bde0112bbf2586
AdemDev <33802588+ademgunay@users.noreply.github.com>
2021-07-22 15:14:53 +0700

    Added loading dialog

---
+++ lib/app/core/widget/loading_dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+extension LoadingDialogGet on GetInterface {
+  Future loadingDialog() {
+    return Get.dialog(
+      Center(
+        child: CircularProgressIndicator(
+          color: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
