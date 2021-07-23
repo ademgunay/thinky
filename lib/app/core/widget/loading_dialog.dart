1195a7c973b86bd63294aa65943c548e8bf48b3a
AdemDev <33802588+ademgunay@users.noreply.github.com>
2021-07-23 11:37:01 +0700

    Updated loading dialog with dismissible parameter

--- lib/app/core/widget/loading_dialog.dart
+++ lib/app/core/widget/loading_dialog.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 extension LoadingDialogGet on GetInterface {
-  Future loadingDialog() {
+  Future loadingDialog({bool dismissible = false}) {
     return Get.dialog(
       Center(
         child: CircularProgressIndicator(
           color: Colors.purple,
         ),
       ),
-      barrierDismissible: false,
+      barrierDismissible: dismissible,
     );
   }
 }
